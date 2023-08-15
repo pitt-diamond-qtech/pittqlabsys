4e28ba008c870bc8d9cd36d56cc1490945775e07
B103-PC <l00055843@CSI362213.anl.gov>
2023-08-15 10:21:50 -0500

    fixed pytest function, ran both ext clock and internal clock for counter read on pxi6733 successfully

--- tests/nidaq_test.py
+++ tests/nidaq_test.py
@@ -13,13 +13,25 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from src.Controller.ni_daq import NIDAQ,PXI6733
+from src.Controller.ni_daq import NIDAQ, PXI6733
 import pytest
 import matplotlib.pyplot as plt
 import time
 import numpy as np
 
-#@pytest.mark.run_this
+
+# @pytest.mark.run_this
+
+@pytest.fixture
+def get_daq() -> NIDAQ:
+    return NIDAQ()
+
+
+@pytest.fixture
+def get_pxi6733() -> PXI6733:
+    return PXI6733()
+
+
 def test_nidaq(capsys):
     daq = NIDAQ()
     dev_list = daq.get_connected_devices()
@@ -27,10 +39,13 @@ def test_nidaq(capsys):
         with capsys.disabled():
             print(d)
 
+
 def test_pxi6733_connection():
     daq = PXI6733()
     assert daq.is_connected
 
+def test_pxi6733_connection2(get_pxi6733):
+    assert get_pxi6733.is_connected
 @pytest.mark.run_this
 def test_pxi6733_ctrout():
     daq = PXI6733()
@@ -41,18 +56,28 @@ def test_pxi6733_ctrout():
     daq.wait_to_finish(clk_task)
     daq.stop(clk_task)
 
-@pytest.mark.parametrize("ext_clock",[True,False])
-def test_pxi6733_ctr_read(capsys,ext_clock):
-    daq = PXI6733()
+@pytest.mark.run_this
+def test_pxi6733_ctrout(get_pxi6733):
+    daq = get_pxi6733
+    clk_task = daq.setup_clock('ctr0', 1000)
+    print('clktask: ', clk_task)
+    time.sleep(0.1)
+    daq.run(clk_task)
+    daq.wait_to_finish(clk_task)
+    daq.stop(clk_task)
+
+@pytest.mark.parametrize("ext_clock", [True, False])
+def test_pxi6733_ctr_read(capsys, get_pxi6733,ext_clock):
+    daq = get_pxi6733
     ctr_task = daq.setup_counter('ctr0', 50, use_external_clock=ext_clock)
     samp_rate = daq.tasklist[ctr_task]['sample_rate']
-    avg_counts_per_bin = np.diff(data).mean()
     time.sleep(0.1)
     daq.run(ctr_task)
     time.sleep(0.1)
-    # daq.wait_to_finish(ctr_task)
     data, nums = daq.read(ctr_task)
     daq.stop(ctr_task)
+    avg_counts_per_bin = np.diff(data).mean()
+    # daq.wait_to_finish(ctr_task)
 
 
     with capsys.disabled():
@@ -61,6 +86,3 @@ def test_pxi6733_ctr_read(capsys,ext_clock):
         print('the sampling rate was {}'.format(samp_rate))
         print("The avg counts per bin was {}".format(avg_counts_per_bin))
         print("The counting rate is {} cts/sec".format(avg_counts_per_bin * samp_rate))
-
-
-
