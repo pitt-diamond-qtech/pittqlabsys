3be301b3db9da2821fb7fd71cd90be87a26c1d62
AbbyBakkenist <aeb200@pitt.edu>
2024-09-26 12:21:00 -0400

    Add files via upload

---
+++ tests/test_pulseblaster.py
@@ -0,0 +1,44 @@
+from src.Controller.pulse_blaster import PulseBlaster
+import time
+import sys
+import struct
+
+"""Run with command line arguments clock (MHz) and runtime (s)"""
+
+input_params = sys.argv
+try:
+    clock = float(input_params[1])
+    run_time = float(input_params[2])
+except IndexError:
+    print ('input_params')
+    print ('NOT ENOUGH INPUT PARAMETERS! SEQUENCE NOT STARTED.')
+except ValueError:
+    print ('input_params')
+    print ('ERROR PARSING INPUT PARAMETERS! INPUTS SHOULD BE NUMBER VALUES.')
+else:
+    print ('clock: ', clock)
+    print ('run_time: ', run_time)
+
+    if struct.calcsize("P") * 8 == 64:
+        libraryFileName = 'spinapi64.dll'
+    else:
+        libraryFileName = 'spinapi.dll'
+    pb = PulseBlaster(libraryFile = libraryFileName)
+    try:
+        pb.close()
+    except RuntimeError:
+        print('Board already closed')
+
+    if pb.init() == None:
+        print('Board initialized')
+    if pb.setclock(clock) == None:
+        print('PB clock set to: %f MHz'% clock)
+    pb.stop()
+    if pb.start() == None:
+        print('Pulse sequence started, running for %f seconds' % run_time)
+    time.sleep(run_time)
+    pb.stop()
+    if pb.close() == None:
+        print('Board closed')
+    print('Pulse sequence finished')
+
