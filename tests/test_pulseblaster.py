276d3548f7730c8185ebef7d02906388dd93cb12
AbbyBakkenist <aeb200@pitt.edu>
2024-10-24 11:27:45 -0400

    Add files via upload

--- tests/test_pulseblaster.py
+++ tests/test_pulseblaster.py
@@ -1,45 +1,21 @@
-from src.Controller.pulse_blaster import PulseBlaster
-import time
-import sys
-import struct
+from src.Controller.pulse_blaster import *
+import pytest
+import numpy as np
+from scipy import signal
+import math
 
-"""This script will initalize the PulseBlaster board and begin running the instructions loaded onto the command line.
-Command line arguments are clock (MHz) and runtime (s)"""
+@pytest.fixture()
+def test_pulse_blaster(get_pci6229, channel):
+    device = PulseBlaster
+    samp_rate = 20000.0
 
-input_params = sys.argv
-try:
-    clock = float(input_params[1])
-    run_time = float(input_params[2])
-except IndexError:
-    print ('input_params')
-    print ('NOT ENOUGH INPUT PARAMETERS! SEQUENCE NOT STARTED.')
-except ValueError:
-    print ('input_params')
-    print ('ERROR PARSING INPUT PARAMETERS! INPUTS SHOULD BE NUMBER VALUES.')
-else:
-    print ('clock: ', clock)
-    print ('run_time: ', run_time)
+    period = 1e-3
+    t_end = period
 
-    if struct.calcsize("P") * 8 == 64:
-        libraryFileName = 'spinapi64.dll'
-    else:
-        libraryFileName = 'spinapi.dll'
-    pb = PulseBlaster(library_file = libraryFileName)
-    try:
-        pb.close()
-    except RuntimeError:
-        print('Board already closed')
-
-    if pb.init() == None:
-        print('Board initialized')
-    if pb.setclock(clock) == None:
-        print('PB clock set to: %f MHz'% clock)
-    pb.stop()
-    if pb.start() == None:
-        print('Pulse sequence started, running for %f seconds' % run_time)
-    time.sleep(run_time)
-    pb.stop()
-    if pb.close() == None:
-        print('Board closed')
-    print('Pulse sequence finished')
-
+    num_samples = math.ceil(t_end * samp_rate)
+    t_array = np.linspace(0, t_end, num_samples)
+    waveform = signal.sawtooth(2 * np.pi * t_array / period, 0.5) + 1
+    ao_task = device.setup_AO([channel],waveform)
+    device.run(ao_task)
+    device.wait_to_finish(ao_task)
+    device.stop(ao_task)
