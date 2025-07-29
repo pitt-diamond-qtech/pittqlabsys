081ca3f86e045047a4f89dbce75d057725b2361f
Gurudev Dutt <55997000+gurudevdutt@users.noreply.github.com>
2025-07-29 18:35:33 -0400

    Remove incomplete odmr.py file
    Removed the incomplete odmr.py file that was started but not finished.
    The new odmr_experiment.py file provides the complete ODMR functionality.

--- src/Model/experiments/odmr.py
+++
@@ -1,7 +0,0 @@
-
-# Created by gurudevdutt at 7/26/25
-import numpy as np
-import time
-from src.core import Parameter, Experiment
-from src.Controller import MicrowaveGenerator, ADwinGold,
-from src.Model.experiments.
